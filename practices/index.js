10e11f5480be71a5e07247b7d96a51ed45925e9d
yusuf ishak damar <ysfishak14@gmail.com>
2024-02-18 20:54:08 +0300

    first commit

---
+++ practices/index.js
@@ -0,0 +1,5 @@
+import {topla, cikar, carp} from "./myModule.js";
+
+console.log("TOPLAMA İŞLEMİ SONUCU : " + (topla(289,14)));
+console.log("ÇIKARMA İŞLEMİ SONUCU : " + (cikar(289,14)));
+console.log("ÇARPMA İŞLEMİ SONUCU : " + (carp(25,14)));
---
+++ practices/myModule.js
@@ -0,0 +1,11 @@
+const topla = (a, b) =>  a+b;
+
+const cikar = (a, b) => {
+    return a-b;
+}
+
+const carp = (a, b) => {
+    return a*b;
+}
+
+export {topla,cikar,carp};

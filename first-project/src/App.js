4449bec3e1ef2810840f78d30d10ea678424e026
yusuf ishak damar <ysfishak14@gmail.com>
2024-02-25 15:24:49 +0300

    static datas

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -9,32 +9,9 @@ return (
 <>
     <h2 className="text-3xl text-center pt-20">İnternet Sitesine Hoşgeldiniz</h2>
     <h4 className="text-2xl pt-20 text-center">KULLANICILAR</h4>
-    <User 
-        name="yusuf"
-        surname="damar" 
-        age={32} 
-        job="Yazılımcı"
-        isLogged={true} 
-        cars={["Volvo","BMW"]} 
-        />
+    <User />
 
-      <User 
-        name="ismail" 
-        surname="damar" 
-        age={46} 
-        job="Muhasebeci"
-        isLogged={true} 
-        cars={["Ford","Chery"]} 
-      />
-
-      <User 
-        name="ismail" 
-        surname="damar" 
-        age={46} 
-        job="Muhasebeci"
-        isLogged={true} 
-        cars={["Ford","Chery"]} 
-      />
+     
 
       <div className='mt-20 bg-gray h-20'></div>
 
--- first-project/src/components/User.js
+++ first-project/src/components/User.js
@@ -1,19 +1,42 @@
 import PropTypes from "prop-types";
 
+
+
+
 function User(props){
+
+const users = [
+    {
+        name: "yusuf",
+        surname: "damar",
+        age: 21,
+        job: "react.js",
+        cars: ["volvo","tesla"]
+    },
+    {
+        name: "kerem",
+        surname: "damar",
+        age: 21,
+        job: "react.js",
+        cars: ["tofaş","volvo"]
+    }
+]
 return (
     <div className="px-24">
+        
+        { users.map(user  => 
         <div className="mb-20 items-center flex flex-col mt-10 border rounded-lg border-black p-4 bg-smoke">
-            <h2 className="ml-10 capitalize">Kullanıcı: {props.name} {props.surname}</h2>
-            <h2 className="ml-10">Meslek: {props.job}</h2>
-            <h2 className="ml-10">Yaş: {props.age}</h2>
+            <h2 className="ml-10 capitalize">Kullanıcı: {user.name} {user.surname}</h2>
+            <h2 className="ml-10">Meslek: {user.job}</h2>
+            <h2 className="ml-10">Yaş: {user.age}</h2>
             <h1 className="ml-10 mt-4 text-2xl">ARAÇLAR</h1>
             <ul>
-            { props.cars.map((car) => (
+            { user.cars.map((car) => (
                 <li> - {car}</li>
             ))}
             </ul>
         </div>
+        )}
     </div>
 )};
 

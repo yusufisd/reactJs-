f362ee0e43e6c06148a2725dcd3660f01d51cb4b
yusuf ishak damar <ysfishak14@gmail.com>
2024-02-26 12:16:18 +0300

    sayac uygulamasi

--- state/src/App.js
+++ state/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [sayac, setSayac] = useState(0);
+  const [alert, setAlert] = useState("");
+  const deneme = (type) => {
+    let lastVal = sayac;
+    if(type == 0){
+      lastVal -= 1;
+    }else{
+      lastVal += 1;
+    }
+
+    if(lastVal < 0){
+      setAlert("sayaç 0'dan küçük değer olamaz.")
+    }else{
+      setSayac(lastVal)
+      setAlert("")
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="items-center mt-40 flex flex-col ml-20">
+        <h1 className="flex mb-10 text-3xl">SAYAÇ</h1>
+        <div className="flex flex-row gap-4 items-center">
+          <button onClick={() => deneme(0) } className="border p-2 bg-black text-white rounded-lg">Azalt (-)</button>
+          <h4 className="text-4xl border px-8 py-2 rounded-lg">{sayac}</h4>
+          <button onClick={() => deneme(1)} className="border p-2 bg-black text-white rounded-lg">Arttır (+)</button>
+        </div>
+        <p>{alert}</p>
     </div>
   );
 }
---
+++ state/tailwind.config.js
@@ -0,0 +1,10 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  plugins: [],
+}
+
+
+
